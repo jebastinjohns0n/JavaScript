c69960b1c6e7cb63d886e8cd1f76cedd7ebf1ed7
jebastinjohns0n <jebastinjohnson7@gmail.com>
2024-01-27 13:11:04 +0530

    26/1/2024 last

---
+++ script.js
@@ -0,0 +1,18 @@
+// function review (){
+   
+// }
+// review()
+
+let mark = prompt( "Enter your Mark" )
+
+if (mark < 50) {
+    ("You need to improve")
+}
+
+else if (mark >= 50 && mark <70 ) {
+    console.log("Good Job")
+}
+
+else if (mark >= 70) {
+    console.log("Excellent perfomance")
+}
